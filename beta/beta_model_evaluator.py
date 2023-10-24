c819ef131f1a1eb2d8af59c0210087bf032dd60c
Mirr2 <mirr2.jo@gmail.com>
2023-10-24 17:57:49 +0900

    delete

--- beta/beta_model_evaluator.py
+++
@@ -1,8 +0,0 @@
-
-
-class ModelEvaluator:
-    def __init__(self, model):
-        self.model = model
-
-    def evaluate_model(self, x_test, y_test):
-        return self.model.evaluate(x_test, y_test)[1]
